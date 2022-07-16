d9f8276bcf065614d63c41260b9cedf81354ab85
Grace Marcoux <gmmarcoux@me.com>
2022-07-16 18:42:33 -0500

    Procfile

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //Dependencies
 const express = require('express');
+const app = express();
 const path = require('path');
 
 //Express App
 const PORT = process.env.PORT || 3001;
-const app = express();
+
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
